9cbe3003e3792c140c3a4febf21c4bdaa3d59ec7
Rihan <me@file.properties>
2022-09-19 23:07:00 +0000

    Layout images properly

--- src/components/UserPost.vue
+++ src/components/UserPost.vue
@@ -1,29 +1,42 @@
 <script setup lang="ts">
 import type { Post } from "@/types/types";
-defineProps<{ post: Post }>();
+const props = defineProps<{ post: Post }>();
+
+const isSecondaryLarge = ref(false);
+
+const imageUrl = (primary: boolean) =>
+  primary ? props.post.photoURL : props.post.secondaryPhotoURL;
+
+const alt = (primary: boolean) =>
+  primary
+    ? `${props.post.userName}'s photo'`
+    : `${props.post.userName}'s secondary photo`;
 </script>
 
 <template>
   <div>
     <UserPostHeader :post="post" />
 
-    <img
-      :src="post.photoURL"
-      class="rounded-xl"
-      :alt="`${post.userName}'s photo`"
-      loading="lazy"
-      :width="post.imageWidth"
-      :height="post.imageHeight"
-    />
+    <div class="relative">
+      <img
+        :src="imageUrl(!isSecondaryLarge)"
+        class="rounded-xl"
+        :alt="alt(!isSecondaryLarge)"
+        loading="lazy"
+        :width="post.imageWidth"
+        :height="post.imageHeight"
+      />
 
-    <img
-      class="rounded-xl mt-1"
-      :src="post.secondaryPhotoURL"
-      :alt="`${post.userName}'s secondary photo`"
-      loading="lazy"
-      :width="post.imageWidth"
-      :height="post.imageHeight"
-    />
+      <img
+        class="rounded-xl mt-3 ml-3 absolute top-0 left-0 w-1/4 border-2 border-black cursor-pointer"
+        :src="imageUrl(isSecondaryLarge)"
+        :alt="alt(isSecondaryLarge)"
+        loading="lazy"
+        :width="post.imageWidth"
+        :height="post.imageHeight"
+        @click="isSecondaryLarge = !isSecondaryLarge"
+      />
+    </div>
 
     <p v-if="post.caption" class="px-3 mt-1 text-md font-medium">
       {{ post.caption }}
