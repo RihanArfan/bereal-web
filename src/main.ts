3d31b1574453f206d412e58e98b8931c7899847e
Rihan <me@file.properties>
2022-09-20 01:47:19 +0000

    SW periodic auto updates

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { VueQueryPlugin } from "vue-query";
 
+import { registerSW } from "virtual:pwa-register";
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -14,3 +16,19 @@ app.use(router);
 app.use(VueQueryPlugin);
 
 app.mount("#app");
+
+const intervalMS = 60 * 60 * 500; // 30 minutes
+
+registerSW({
+  onRegisteredSW(swUrl, r) {
+    r &&
+      setInterval(async () => {
+        if (!(!r.installing && navigator)) return;
+        if ("connection" in navigator && !navigator.onLine) return;
+
+        const resp = await fetch(swUrl, { cache: "no-store" });
+
+        if (resp?.status === 200) await r.update();
+      }, intervalMS);
+  },
+});
