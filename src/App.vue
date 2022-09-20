2f09222061959fc3dd6a0a67015dfc2760264c18
Rihan <me@file.properties>
2022-09-20 00:17:09 +0000

    Begin sticky nav

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header class="mb-5">
+  <header
+    class="mb-3 sticky top-0 z-50 py-2 bg-black/[97%] before:content-[''] before:absolute before:rounded-tl-lg before:shadow-black/[97%] before:shadow-[0_-25px_0_0] before:h-[50px] before:bottom-[-50px] before:w-3 after:content-[''] after:absolute after:rounded-tr-lg after:shadow-black/[97%] after:shadow-[0_-25px_0_0] after:h-[50px] after:bottom-[-50px] after:w-3 after:right-0"
+  >
     <img
       alt="BeReal logo"
       src="@/assets/text.png"
