8ba30c9f4fc640eea22b06f100e06e9bd93e46d8
Rihan <me@file.properties>
2022-09-26 11:15:49 +0000

    Improve navigation

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
   <NavBar />
-  <RouterView />
+
+  <div class="max-w-lg mx-auto px-2 pb-2 mt-14">
+    <RouterView />
+  </div>
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,25 +7,27 @@ const { account } = storeToRefs(accountStore);
 </script>
 <template>
   <header
-    class="flex justify-between items-center px-4 mb-3 sticky top-0 z-50 py-2 bg-black/[97%] before:content-[''] before:absolute before:rounded-tl-lg before:shadow-black/[97%] before:shadow-[0_-25px_0_0] before:h-[50px] before:bottom-[-50px] before:w-3 before:left-0 after:content-[''] after:absolute after:rounded-tr-lg after:shadow-black/[97%] after:shadow-[0_-25px_0_0] after:h-[50px] after:bottom-[-50px] after:w-3 after:right-0"
+    class="max-w-lg mx-auto left-0 right-0 pb-3 fixed w-full z-10 top-0 bg-gradient-to-b from-black to-transparent"
   >
-    <div class="w-8" />
+    <nav class="flex justify-between items-center px-4 top-0 z-50 pt-2">
+      <div class="w-8" />
 
-    <RouterLink :to="{ name: 'friends-feed' }">
-      <img
-        alt="BeReal logo"
-        src="@/assets/text.png"
-        class="invert mx-auto"
-        width="100"
-      />
-    </RouterLink>
+      <RouterLink :to="{ name: 'friends-feed' }">
+        <img
+          alt="BeReal logo"
+          src="@/assets/text.png"
+          class="invert mx-auto"
+          width="90"
+        />
+      </RouterLink>
 
-    <RouterLink :to="{ name: 'account' }">
-      <UserIcon
-        :profile-picture="account?.profilePicture"
-        :username="account?.username ?? ''"
-        :size="8"
-      />
-    </RouterLink>
+      <RouterLink :to="{ name: 'account' }">
+        <UserIcon
+          :profile-picture="account?.profilePicture"
+          :username="account?.username ?? ''"
+          :size="8"
+        />
+      </RouterLink>
+    </nav>
   </header>
 </template>
