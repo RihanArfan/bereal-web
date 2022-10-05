cc55bb50a9e1fdb6aa83f53ed385e0686154805c
Rihan <me@file.properties>
2022-10-05 13:05:22 +0000

    Add delay to grow animation

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -61,7 +61,7 @@ const profile = computed(() => {
       :profile-picture="profile?.profilePicture"
       :username="profile?.username ?? route.params.username"
       :size="32"
-      class="border-4 border-black transition-all hover:-mt-14 hover:h-64 hover:w-64"
+      class="border-4 border-black transition-all hover:-mt-14 hover:h-64 hover:w-64 sm:delay-150"
     />
 
     <h1 class="mt-2 text-2xl font-semibold">
