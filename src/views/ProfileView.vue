5d2f58eeaff555435fd466af3d608025947929d8
Rihan <me@file.properties>
2022-10-09 20:42:22 +0000

    Separate my profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,49 +1,42 @@
 <script setup lang="ts">
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { storeToRefs } from "pinia";
-import { useQuery } from "vue-query";
 import { useRoute } from "vue-router";
-
-import { useApi, useApiFunction } from "@/composables/useApi";
-import { useAccountStore } from "@/stores/account";
-import type { Profile } from "@/types/types";
-import type { GetUserProfile } from "@/types/functions";
 
 dayjs.extend(relativeTime);
 
 const accountStore = useAccountStore();
-const { account } = storeToRefs(accountStore);
-
 const route = useRoute();
 
 const isMyProfile = computed(
-  () =>
-    account.value?.username && account.value?.username === route.params.username
+  () => accountStore.account?.username === route.params.username
 );
 
-const profileFetcher = async () => {
-  // TODO: find user ID from cache of vue query before checking function
-  const user = await useApiFunction()
-    .post("getUserProfile", {
-      json: { data: { userName: route.params.username } },
-    })
-    .json<GetUserProfile>();
+const {
+  isLoading: isSearchLoading,
+  isError: isSearchError,
+  data: searchResults,
+} = useSearchProfileQuery(route.params.username as string, {
+  enabled: !isMyProfile.value,
+});
 
-  return await useApi()
-    .get(`person/profiles/${user.result.uid}`)
-    .json<Profile>();
-};
+const profileId = computed(() => {
+  return searchResults.value?.data.find(
+    (result) => result.username === route.params.username
+  )?.id;
+});
 
-const fetchProfile = () =>
-  useQuery(["profile", route.params.username], profileFetcher, {
-    enabled: !isMyProfile.value,
-  });
-const { isLoading, isError, data, error } = fetchProfile();
+const isProfileReady = computed(() => {
+  return typeof profileId.value !== "undefined";
+});
 
-const profile = computed(() => {
-  if (isMyProfile.value) return account.value;
-  return data.value;
+const {
+  isLoading,
+  isError,
+  data: profile,
+  error,
+} = useProfileQuery(profileId, {
+  enabled: isProfileReady && !isMyProfile.value,
 });
 </script>
 
@@ -65,8 +58,10 @@ const profile = computed(() => {
     />
 
     <h1 class="mt-2 text-2xl font-semibold">
-      <template v-if="isLoading && !isMyProfile">Loading...</template>
-      <template v-else-if="isError">Profile Not Found</template>
+      <template v-if="isSearchLoading || isLoading">Loading...</template>
+      <template v-else-if="isSearchError || isError || !profileId">
+        Profile Not Found
+      </template>
       <template v-else>{{ profile?.fullname }}</template>
     </h1>
 
@@ -82,32 +77,7 @@ const profile = computed(() => {
 
     <p class="mt-1 mb-5">{{ profile?.biography }}</p>
     <p v-if="error" class="mt-1 mb-5">{{ error }}</p>
+
+    {{ !isMyProfile }}
   </div>
-
-  <template v-if="account?.username === $route.params.username">
-    <div class="mb-3 rounded-lg bg-zinc-900 p-3">
-      <div class="mb-1 flex justify-between">
-        <h2 class="mb-2 text-lg font-semibold">Memories</h2>
-        <p class="mt-1 text-xs text-zinc-600">Only visible to you</p>
-      </div>
-
-      <MyMemories />
-    </div>
-
-    <div class="rounded-lg bg-zinc-900 p-3">
-      <div class="mb-1 flex justify-between">
-        <h2 class="mb-2 text-lg font-semibold">RealMojis</h2>
-        <p class="mt-1 text-xs text-zinc-600">Only visible to you</p>
-      </div>
-
-      <div class="flex flex-wrap justify-center gap-2">
-        <RealmojiWithIcon
-          v-for="realmoji in account?.realmojis"
-          :key="realmoji.emoji"
-          class="w-[calc(16.666667%_-_theme('gap.2'))]"
-          :realmoji="realmoji"
-        />
-      </div>
-    </div>
-  </template>
 </template>
