8061268fa1ba10cf01bf4b38e152ab9c8050f0f2
Rihan <me@file.properties>
2022-10-09 20:31:57 +0000

    Fix spacing

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <NavBar />
 
   <div
-    class="mx-auto max-w-lg px-2 pb-2 sm:px-0"
+    class="mx-auto flex max-w-lg flex-col px-2 pb-2 sm:px-0"
     :class="{ 'mt-12': $route.name !== 'profile' }"
   >
     <RouterView />
