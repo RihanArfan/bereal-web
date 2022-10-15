6efefafb05786732f1c70e8831a7645b898384e9
Rihan <me@file.properties>
2022-10-15 02:24:31 +0000

    Spacing improvements

--- src/views/MyPostCaptionView.vue
+++ src/views/MyPostCaptionView.vue
@@ -14,7 +14,11 @@ const {
 </script>
 
 <template>
-  <SkeletonMyPost v-if="isLoading" :hide-details="true" class="mt-12 w-2/3" />
+  <SkeletonMyPost
+    v-if="isLoading"
+    :hide-details="true"
+    class="mx-auto mt-12 w-2/3"
+  />
 
   <span v-else-if="isError">Error: {{ error }}</span>
   <p v-else-if="!data">Post a BeReal to add a caption</p>
@@ -24,7 +28,7 @@ const {
     :post="data"
     :hide-realmojis="true"
     :hide-details="true"
-    class="mx-auto mt-16 w-2/3"
+    class="mx-auto mt-10 w-2/3"
   />
 
   <div
@@ -34,7 +38,7 @@ const {
     <input
       id="caption"
       v-model.lazy="caption"
-      class="w-full border-t-2 border-zinc-800 bg-transparent px-3 py-2 placeholder:text-zinc-500 sm:rounded-lg sm:border-2 sm:bg-zinc-900"
+      class="w-full border-t-2 border-zinc-800 bg-transparent px-3 py-3 placeholder:text-zinc-500 sm:rounded-lg sm:border-2 sm:bg-zinc-900"
       placeholder="Add a caption"
       focus
     />
