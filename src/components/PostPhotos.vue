2026a42c8d8e0d33a167297370f952d0403242d9
Rihan <me@file.properties>
2022-10-25 00:59:18 +0000

    Remove poor hover animation

--- src/components/PostPhotos.vue
+++ src/components/PostPhotos.vue
@@ -30,7 +30,7 @@ const alt = (primary: boolean) =>
     />
 
     <img
-      class="absolute top-0 left-0 mt-3 ml-3 w-1/3 cursor-pointer rounded-xl border-2 border-black bg-zinc-800 transition-all hover:opacity-90"
+      class="absolute top-0 left-0 mt-3 ml-3 w-1/3 cursor-pointer rounded-xl border-2 border-black bg-zinc-800"
       :src="imageUrl(isSecondaryLarge)"
       :alt="alt(isSecondaryLarge)"
       loading="lazy"
