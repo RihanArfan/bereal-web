6a6677f2f7f287200eca086e7994c30a5b462444
Rihan <me@file.properties>
2022-10-25 02:33:36 +0000

    Smooth shadow at larger viewports

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,7 +7,7 @@ const { account } = storeToRefs(accountStore);
 </script>
 <template>
   <header
-    class="fixed left-0 right-0 top-0 z-10 mx-auto w-full max-w-lg shadow-[inset_0_30px_30px_-15px_rgba(0,0,0,0.8)]"
+    class="fixed left-0 right-0 top-0 z-10 mx-auto w-full max-w-lg shadow-[inset_0_30px_30px_-15px_rgba(0,0,0,0.8)] sm:shadow-[inset_0_28px_28px_-17px_rgba(0,0,0,0.7)]"
   >
     <nav
       class="align-center top-0 z-50 flex items-center justify-between px-4 pt-2"
