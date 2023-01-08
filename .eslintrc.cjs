04f03b9f68bf0dc6eb09355a9ffe82594405ae98
Rihan <me@file.properties>
2023-01-08 04:59:12 +0000

    Add TanStack Query ESLint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
+    "plugin:@tanstack/eslint-plugin-query/recommended",
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
