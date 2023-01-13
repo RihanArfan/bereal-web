9ecd207504e701436e92474b67f720137982872b
Rihan <me@file.properties>
2023-01-13 22:20:07 +0000

    Add QueryCache to auto imports

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
             "useInfiniteQuery",
             "useMutation",
             "useQueryClient",
+            "QueryCache",
           ],
         },
       ],
