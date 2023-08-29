1304c920ff8554bac249e3b7b0c61210feee0776
Rihan <me@file.properties>
2023-08-29 12:40:13 +0000

    refactor: :lipstick: use nuxt ui for icons

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex w-full items-center justify-between px-5 py-3">
-    <Icon
-      name="heroicons:users-20-solid"
-      color="white"
-      class="h-5 w-5"
-    />
+    <UIcon name="i-heroicons-users-20-solid" color="white" class="h-5 w-5" />
 
     <img alt="BeReal Logo" src="/logo.svg" width="100" />
 
-    <Icon name="heroicons:ellipsis-vertical-20-solid" color="white" class="h-5 w-5" />
+    <UIcon
+      name="i-heroicons-ellipsis-vertical-20-solid"
+      color="white"
+      class="h-5 w-5"
+    />
   </div>
 </template>
