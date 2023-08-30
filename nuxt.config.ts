a04dd435c2fa7327f889dcacb81152524aa9ee9c
Rihan <me@file.properties>
2023-08-30 16:24:06 +0000

    feat: :lipstick: enforce dark mode on nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -129,6 +129,10 @@ export default defineNuxtConfig({
     },
   },
 
+  colorMode: {
+    preference: "dark",
+  },
+
   runtimeConfig: {
     berealApiEndpoint: "",
     berealAuthRequestEndpoint: "",
