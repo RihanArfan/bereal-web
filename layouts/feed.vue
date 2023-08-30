44e3b317a5750fc055db86937f826a0036a30c14
Rihan <me@file.properties>
2023-08-30 16:30:03 +0000

    feat(feed): :lipstick: spacing improvements

--- layouts/feed.vue
+++ layouts/feed.vue
@@ -10,10 +10,10 @@ const { isPending, isError, data, error } = useQuery({
 <template>
   <div class="flex text-zinc-100 flex-col md:flex-row">
     <aside
-      class="flex h-screen bg-neutral-900 md:w-1/3 md:min-w-[24rem] md:sticky top-0"
+      class="flex md:h-screen md:bg-neutral-900 md:w-1/3 md:min-w-[24rem] md:sticky top-0"
     >
       <div
-        class="py-2 px-2 md:pl-0 xl:max-w-2xl mx-auto md:mx-0 md:ml-auto flex flex-col w-full"
+        class="p-2 xl:max-w-2xl mx-auto md:mx-0 md:ml-auto flex flex-col w-full"
       >
         <NavBar />
 
@@ -29,8 +29,8 @@ const { isPending, isError, data, error } = useQuery({
       </div>
     </aside>
 
-    <main class="lg:px-3 pt-5 grow flex justify-center">
-      <div class="max-w-md xl:max-w-lg">
+    <main class="md:px-3 pt-5 grow flex justify-center">
+      <div class="sm:max-w-md xl:max-w-lg">
         <slot />
       </div>
     </main>
