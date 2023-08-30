e3e3e4594a393e29f6d73ba565503e7ff2f824de
Rihan <me@file.properties>
2023-08-30 15:07:18 +0000

    fix: :bug: prevent layout shift

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,13 @@
   <div class="flex w-full items-center justify-between px-5 py-3">
     <UIcon name="i-heroicons-users-20-solid" color="white" class="h-5 w-5" />
 
-    <img alt="BeReal Logo" src="/logo.svg" width="100" draggable="false" />
+    <img
+      alt="BeReal Logo"
+      src="/logo.svg"
+      width="100"
+      height="22.5"
+      draggable="false"
+    />
 
     <UIcon
       name="i-heroicons-ellipsis-vertical-20-solid"
