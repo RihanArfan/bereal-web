08aea0dc74a49acb5692eeffffb676508af4c6ec
Rihan <me@file.properties>
2023-08-30 15:06:03 +0000

    feat(profile): :sparkles: profile placeholder page

--- pages/@[user].vue
+++ pages/@[user].vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+  <div class="flex justify-center items-center h-screen flex-col">
+    <h1 class="font-bold text-2xl">@{{ $route.params.user }}</h1>
+    <p class="text-lg mt-1">Profiles coming soon</p>
+
+    <NuxtLink to="/" class="mt-5">
+      <UButton
+        icon="i-heroicons-arrow-left-20-solid"
+        size="lg"
+        color="white"
+        label="Go Home"
+        :trailing="false"
+      />
+    </NuxtLink>
+  </div>
+</template>
