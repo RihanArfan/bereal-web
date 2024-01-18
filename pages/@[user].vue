ca61b495f1e4c6719a205521fe878be6704fb2d3
Rihan Arfan <me@file.properties>
2024-01-18 16:09:06 +0000

    refactor(profile): :building_construction: refactor to shadcn

--- pages/@[user].vue
+++ pages/@[user].vue
@@ -1,16 +1,17 @@
+<script setup lang="ts">
+const route = useRoute("@user");
+</script>
+
 <template>
   <div class="flex justify-center items-center h-screen flex-col">
-    <h1 class="font-bold text-2xl">@{{ $route.params.user }}</h1>
+    <h1 class="font-bold text-2xl">@{{ route.params.user }}</h1>
     <p class="text-lg mt-1">Profiles coming soon</p>
 
     <NuxtLink to="/" class="mt-5">
-      <UButton
-        icon="i-heroicons-arrow-left-20-solid"
-        size="lg"
-        color="white"
-        label="Go Home"
-        :trailing="false"
-      />
+      <Button>
+        <Icon name="heroicons:arrow-left-20-solid" class="mr-2" />
+        Go Home
+      </Button>
     </NuxtLink>
   </div>
 </template>
